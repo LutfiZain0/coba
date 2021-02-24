beb88a3f41bcb8b54bdd4fe2066a3e67e788fdb6
lutfiZain0 <lutfizain8@gmail.com>
2021-02-24 15:32:46 +0700

    php dasar

---
+++ coba.php
@@ -0,0 +1,12 @@
+<?php
+    // definisikan variables
+     $nama = 'Lutfi';
+     $umur = 20;
+     $berat = 50.4;
+
+     echo 'Nama : ' . $nama;
+     echo '<br/>Umur : ' . $umur.' Tahun';
+     echo '<br/>Berat : '.$berat.' Kg';
+
+     echo "<br/>Hello $nama Apakabar";
+?>
